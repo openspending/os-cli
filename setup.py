d855dd40de382769b207bc1a5d043745e845ce11
roll <roll@respect31.com>
2015-11-20 11:03:21 +0300

    simplified setup.py-metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-# from __future__ import unicode_literals
+from __future__ import unicode_literals
 
 import os
 import io
@@ -9,64 +9,32 @@ import json
 from setuptools import setup, find_packages
 
 
-DIR = os.path.abspath(os.path.dirname(__file__))
-README = 'README.md'
-METADATA = 'METADATA.json'
-LICENSE = 'LICENSE.txt'
-README_PATH = os.path.join(DIR, README)
-METADATA_PATH = os.path.join(DIR, METADATA)
+# Prepare
+basedir = os.path.dirname(__file__)
+package = json.load(open(os.path.join(basedir, 'package.json')))
+readme = open(os.path.join(basedir, 'README.md')).read()
+license = open(os.path.join(basedir, 'LICENSE.txt')).read()
+requirements = open(os.path.join(basedir, 'requirements.txt')).read().split()
+requirements_dev = open(os.path.join(basedir, 'requirements.dev.txt')).read().split()
 
 
-with io.open(README_PATH, mode='r+t', encoding='utf-8') as stream:
-    long_description = stream.read()
-
-
-with io.open(METADATA_PATH, mode='r+t', encoding='utf-8') as stream:
-    metadata = json.loads(stream.read())
-
-
-dependencies = [
-    'click',
-    'requests',
-    'requests-futures',
-    'datapackage-validate',
-    'goodtables',
-    'jtskit<0.5',
-    'boto'
-]
-
-
-classifiers = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Web Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    'Topic :: Software Development :: Libraries :: Python Modules'
-]
-
+# Run
 setup(
-    name=metadata['name'],
-    version=metadata['version'],
-    description=metadata['description'],
-    long_description=long_description,
-    author=metadata['author'],
-    author_email=metadata['author_email'],
-    url=metadata['url'],
-    license=metadata['license'],
+    name=package['name'],
+    version=package['version'],
+    description=package['description'],
+    long_description=readme,
+    author=package['author'],
+    author_email=package['author_email'],
+    url=package['repository'],
+    license=package['license'],
     packages=find_packages(exclude=['examples', 'tests']),
-    package_data={'': [README, METADATA, LICENSE]},
-    package_dir={metadata['slug']: metadata['slug']},
+    package_dir={package['slug']: package['slug']},
     include_package_data=True,
-    install_requires=dependencies,
+    install_requires=requirements,
+    tests_require=requirements_dev,
     zip_safe=False,
-    keywords=metadata['keywords'],
+    keywords=package['keywords'],
     classifiers=classifiers,
     entry_points={
         'console_scripts': [
